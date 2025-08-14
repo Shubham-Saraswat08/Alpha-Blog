8bbd88249dc3f469673248487cd5a9f83350449a
Shubham Saraswat <shubhamsaraswat89055@gmail.com>
2025-08-14 19:55:19 +0530

    Implement CRUD Functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,49 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
   def index
     @articles = Article.all
   end
+
   def show
-    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article created Successfully"
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "Article Updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: "Article deleted successfully."
+  end
+end
+
+private
+
+def set_article
+  @article = Article.find(params[:id])
+end
+
+def article_params
+  params.require(:article).permit(:title, :description)
 end
