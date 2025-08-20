869a731e2ebc46f9ae97299b0a995c6679c96869
Shubham Saraswat <shubhamsaraswat89055@gmail.com>
2025-08-20 12:06:19 +0530

    Implement Sign Up Page in user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#home"
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [ :new ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
