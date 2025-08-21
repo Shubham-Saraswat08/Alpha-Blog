69b5a200be42dea1ae849e99000910a2c9c33bed
Shubham Saraswat <shubhamsaraswat89055@gmail.com>
2025-08-21 13:15:44 +0530

    Implement Pagination and session_controller in AlphaBlog

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "#{@user.username} your Profile has been updated Successfully"
-      redirect_to users_path
+      redirect_to @user
     else
       render :edit, status: :unprocessable_entity
     end
@@ -29,10 +29,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 6)
   end
 
   private
